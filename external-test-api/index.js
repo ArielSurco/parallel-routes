88d38c332737a146558b4b640706a3bdd8e7e6ea
ArielSurco <asurco@midasconsultores.com.ar>
2023-09-14 14:35:10 -0300

    Initial commit

---
+++ external-test-api/index.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const app = express();
+const port = 3001;
+
+app.get('/', (req, res) => {
+  const error = false;
+
+  if(error) {
+    res.status(500).send('Broken!');
+  } else {
+    res.status(200).json({
+      message: 'Working!'
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log('Start server in port', port)
+})
